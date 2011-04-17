0c7bdcfb298b61607a156d1cb4fbed786172c112
Jason Wong <jcwwong@ucdavis.edu>
2011-04-16 17:39:53 -0700

    sucessfully itterates through alements within arrray

--- mulsym.s
+++ mulsym.s
@@ -18,8 +18,8 @@ multiplicand:	#multiplicand matrix
 	.long	5
 	.long	6
 
-row:	#current cow
-	.long 1
+row:	#current row
+	.long 1	
 
 col:	#current column
 	.long 1
@@ -39,7 +39,7 @@ mulsym:
 row_op:
 	
 	addl $1, row #after row operations finish, increase row by one
-	coml row, %edx
+	cmpl row, %edx
 	jl inc_col #if rows are larger than dimensions, move to next col
 	jge row_op	#else repeat the row ops
 
@@ -58,14 +58,16 @@ init:
 	movl $3, %edx	#copy matrix size into register
 	movl $multiplier, %esi	#copy multiplier matrix into register
 	movl $multiplicand, %edi	#copy multiplicand matrix into register
+	movl $1, row
+	movl $1, col
 	ret
 
 inc_col:
 	movl $1, row #reset rows to 1
 	addl $1, col #add 1 to columns
-	coml col, %edx
+	cmpl col, %edx
 	jl done	#if the columns is larger than the dimensions, the program is done
-	jge row_ops	#if colums is equal or less than the dimensions, repeat the row ops
+	jge row_op	#if colums is equal or less than the dimensions, repeat the row ops
 	
 
 done:	
